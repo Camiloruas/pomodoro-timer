b089da74cbce629f27bda7714ab8c54e36733c40
Camilo Ruas <miloruas@gmail.com>
2025-07-15 21:25:35 -0300

    component creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,18 @@
-import { Heading } from "./components/Heading";
 import { Container } from "./components/Container";
+import { Heading } from "./components/Heading";
 import "./styles/global.css";
 import "./styles/theme.css";
 
 export function App() {
   return (
     <>
-      <Container> 
-        <Heading> Testando meu Componente</Heading>
-        <section> LOGO</section>
+      <Container>
+        <Heading> LOGO</Heading>
       </Container>
 
-      <Container> 
-        <section> Menu</section>
+      <Container>
+        <Heading> MENU</Heading>
       </Container>
-
-
-      <div className="container">
-        <div className=" content">
-          <section>FORM</section>
-        </div>
-      </div>
-
-      <div className="container">
-        <div className=" content">
-          <section>FOOTER</section>
-        </div>
-      </div>
     </>
   );
 }
