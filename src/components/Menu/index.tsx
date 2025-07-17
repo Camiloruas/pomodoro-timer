5331045c0c098abc7f1e0fc9db14ae07ea55835d
Camilo Ruas <miloruas@gmail.com>
2025-07-16 22:12:45 -0300

    Styling the menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,18 +1,24 @@
-import { History, House, Settings, Sun } from "lucide-react";
+import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
 
 import styles from "./styles.module.css";
 
 export function Menu() {
   return (
-    <div>
+    <nav>
       <div className={styles.menu}>
         <a className={styles.menuLink} href="#">
-          <House />
-          <History />
-          <Settings />
-          <Sun />
+          <HouseIcon />
+        </a>
+        <a className={styles.menuLink} href="#">
+          <SettingsIcon />
+        </a>
+        <a className={styles.menuLink} href="#">
+          <SunIcon />
+        </a>
+        <a className={styles.menuLink} href="#">
+          <HistoryIcon />
         </a>
       </div>
-    </div>
+    </nav>
   );
 }
