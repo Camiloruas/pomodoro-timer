d68157d332991bd4a6ac5678b1046356857f34e7
Camilo Ruas <miloruas@gmail.com>
2025-08-05 23:21:51 -0300

    Organizing context into separate files

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,13 +1,54 @@
 import { PlayCircleIcon } from "lucide-react";
-import { Button } from "../Button";
 import { Cycles } from "../Cycles";
+import { Button } from "../Button";
 import { Input } from "../Input";
+import { useRef } from "react";
+import type { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
 
 export function MainForm() {
+  const { state, setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  // ciclos
+  const nextCycle = getNextCycle(state.currentCycle);
+
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    console.log("DEU CERTO");
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert("Digite o nome da tarefa");
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: "workTime",
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState((prevState) => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining, // Conferir
+        formattedSecondsRemaining: "00:00", // Conferir
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
   }
 
   return (
@@ -18,11 +59,12 @@ export function MainForm() {
           id="meuInput"
           type="text"
           placeholder="Digite algo"
+          ref={taskNameInput}
         />
       </div>
 
       <div className="formRow">
-        <p>Próximo intervalo é de 25 min</p>
+        <p>Próximo intervalo é de 25min</p>
       </div>
 
       <div className="formRow">
