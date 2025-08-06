dd4712445a4b37b38afb5feea4aff526b4164f0d
Camilo Ruas <miloruas@gmail.com>
2025-08-06 10:41:03 -0300

    render cycles with dynamic classes and accessibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { Home } from "./pages/Home";
-import { TaskContextProvider } from "./contexts/TaskContext";
+import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
 import "./styles/global.css";
 import "./styles/theme.css";
-
 
 export function App() {
   return (
     <TaskContextProvider>
-
-    <Home />
+      <Home />
     </TaskContextProvider>
-);
+  );
 }
--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -6,6 +6,8 @@ import { useRef } from "react";
 import type { TaskModel } from "../../models/TaskModel";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCyclesType";
+import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
 
 export function MainForm() {
   const { state, setState } = useTaskContext();
@@ -13,6 +15,7 @@ export function MainForm() {
 
   // ciclos
   const nextCycle = getNextCycle(state.currentCycle);
+  const nextCyleType = getNextCycleType(nextCycle);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -32,8 +35,8 @@ export function MainForm() {
       startDate: Date.now(),
       completeDate: null,
       interruptDate: null,
-      duration: 1,
-      type: "workTime",
+      duration: state.config[nextCyleType],
+      type: nextCyleType,
     };
 
     const secondsRemaining = newTask.duration * 60;
@@ -45,7 +48,7 @@ export function MainForm() {
         activeTask: newTask,
         currentCycle: nextCycle,
         secondsRemaining, // Conferir
-        formattedSecondsRemaining: "00:00", // Conferir
+        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining), // Conferir
         tasks: [...prevState.tasks, newTask],
       };
     });
@@ -55,7 +58,7 @@ export function MainForm() {
     <form onSubmit={handleCreateNewTask} className="form" action="">
       <div className="formRow">
         <Input
-          labelText="task"
+          LabelTex="task"
           id="meuInput"
           type="text"
           placeholder="Digite algo"
@@ -67,9 +70,11 @@ export function MainForm() {
         <p>Próximo intervalo é de 25min</p>
       </div>
 
-      <div className="formRow">
-        <Cycles />
-      </div>
+      {state.currentCycle > 0 && (
+        <div className="formRow">
+          <Cycles />
+        </div>
+      )}
 
       <div className="formRow">
         <Button icon={<PlayCircleIcon />} />
