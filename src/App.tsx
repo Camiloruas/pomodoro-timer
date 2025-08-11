d5e1426d5c13374243cc7bba51d3c8fc72c6e5c2
Camiloruas <miloruas@gmail.com>
2025-08-11 17:08:01 -0300

    React Toastify - Adding Beautiful User Messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import { Home } from "./pages/Home";
 import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
 import "./styles/global.css";
-import "./styles/theme.css";
+import "./styles/theme.css"; // Assuming this is for custom theme styles
+import { ToastContainer, Bounce } from 'react-toastify';
 
 export function App() {
   return (
     <TaskContextProvider>
       <Home />
+      <ToastContainer
+        position="top-center"
+        autoClose={10000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={true}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
     </TaskContextProvider>
   );
 }
