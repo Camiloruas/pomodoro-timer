6fdddd1159b6b69755f983e74e257e876bf4436e
Camiloruas <miloruas@gmail.com>
2025-08-12 15:24:20 -0300

    Toastify deployment complete

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,15 @@ import { Home } from "./pages/Home";
 import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
 import "./styles/global.css";
 import "./styles/theme.css"; // Assuming this is for custom theme styles
-import { ToastContainer, Bounce } from 'react-toastify';
+import { MessagesContainer } from "./components/MessagesContainer";
 
 export function App() {
   return (
     <TaskContextProvider>
-      <Home />
-      <ToastContainer
-        position="top-center"
-        autoClose={10000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick={true}
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-        transition={Bounce}
-      />
+      <MessagesContainer>
+        <Home />
+      </MessagesContainer>
     </TaskContextProvider>
   );
 }
+  
