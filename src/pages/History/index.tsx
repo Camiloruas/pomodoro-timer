3b7fe2c48f18250badc9d0469e34f75f3277623c
Camiloruas <miloruas@gmail.com>
2025-08-15 18:30:29 -0300

    Creating the History page structure and CSS

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,30 +1,56 @@
 import { TrashIcon } from "lucide-react";
 import { Container } from "../../components/Container";
-import { Heading } from "../../components/Heading";
-import { MainTemplate } from "../../components/templates/MainTemplates";
 import { Button } from "../../components/Button";
+import { Heading } from "../../components/Heading";
+import { MainTemplate } from "../../../src/components/templates/MainTemplates";
+
 import styles from "./styles.module.css";
 
 export function History() {
-  // const { state, setState } = props;
   return (
     <MainTemplate>
       <Container>
         <Heading>
           <span>History</span>
-          <span className={styles.buttinContainer}>
+          <span className={styles.buttonContainer}>
             <Button
               icon={<TrashIcon />}
               color="red"
-              arial-label="Apagar todo o Histórico"
-              title="Apagar Histórico"
+              aria-label="Apagar todo o histórico"
+              title="Apagar histórico"
             />
           </span>
         </Heading>
       </Container>
 
       <Container>
-        <div className="reposiTable"> sdafasdfasdfadsf</div>
+        <div className={styles.responsiveTable}>
+          <table>
+            <thead>
+              <tr>
+                <th>Tarefa</th>
+                <th>Duração</th>
+                <th>Data</th>
+                <th>Status</th>
+                <th>Tipo</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {Array.from({ length: 20 }).map((_, index) => {
+                return (
+                  <tr key={index}>
+                    <td>Estudar</td>
+                    <td>25min</td>
+                    <td>20/04/2025 08:00</td>
+                    <td>Completa</td>
+                    <td>Foco</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </Container>
     </MainTemplate>
   );
